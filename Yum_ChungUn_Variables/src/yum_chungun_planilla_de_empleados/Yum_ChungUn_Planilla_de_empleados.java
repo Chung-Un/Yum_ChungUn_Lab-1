52db07b7090bf5f824234fc35e7b5c8305e34453
Chung-Un <chungunyum23@gmail.com>
2024-10-18 08:54:54 -0600

    Finalizacion
    Correcciones esteticas e impresion de la boleta del empleado con los datos obtenidos previamente.

--- Yum_ChungUn_Variables/src/yum_chungun_planilla_de_empleados/Yum_ChungUn_Planilla_de_empleados.java
+++ Yum_ChungUn_Variables/src/yum_chungun_planilla_de_empleados/Yum_ChungUn_Planilla_de_empleados.java
@@ -15,16 +15,19 @@ public class Yum_ChungUn_Planilla_de_empleados {
         float tarifa_por_hora;
         double salario_semanal;
         
-        System.out.println("**Programa para la realizacion de boleta del empleado/nIngrese su nombre completo: ");
+        System.out.println("**Programa para la realizacion de boleta del empleado\nIngrese su nombre completo: ");
         nombre_empleado= entrada.nextLine();
         
         System.out.println("Ingrese su cantidad de horas mensuales: ");
         horas_mensuales= entrada.nextInt();
         
-        System.out.println("Ingrese su salario por hora: Lps. ");
+        System.out.println("Ingrese su salario por hora: Lps.");
         tarifa_por_hora = entrada.nextInt();
         
         salario_semanal = (tarifa_por_hora*horas_mensuales)/4;
+        
+        System.out.println("----- Boleta del Empleado -------\nNombre del empleado: " + nombre_empleado + "\nHora de trabajo mensual: "+ horas_mensuales + 
+                " horas\nTarifa por hora: Lps." + tarifa_por_hora+ " por hora\nSalario del empleado semanal: Lps." + salario_semanal);
         
         
     }

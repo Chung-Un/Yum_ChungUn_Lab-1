3a7f3971f6d24e6f43f7d411f4d618480b2ff965
Chung-Un <chungunyum23@gmail.com>
2024-10-18 09:11:47 -0600

    Asignacion de variables
    Asignacion de variables necesarios, además, inicializacion del scanner.

---
+++ Yum_ChungUn_Cuotas/src/yum_chungun_cuotas/Yum_ChungUn_Cuotas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yum_chungun_cuotas;
+
+import java.util.Scanner;
+
+public class Yum_ChungUn_Cuotas {
+
+    
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);
+       int plazo_pago,interesmensual, comision_cuota, seguro_mensual;
+       double total_pagar,cuota_mensual;
+       
+       
+    }
+    
+}

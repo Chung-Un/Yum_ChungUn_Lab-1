75b876b8e47e86d19e191cc4e21e3941bc1267fd
Chung-Un <chungunyum23@gmail.com>
2024-10-18 09:55:12 -0600

    Inicio
    Se inicializa el scanner, asignan los variables y se le pide cuantos clics recibio el usuario. Ahora se asignara el costo invidivual de los clics en base a cuantos recibio.

---
+++ Yum_ChungUn_Clics/src/yum_chungun_clics/Yum_ChungUn_Clics.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yum_chungun_clics;
+
+/**
+ *
+ * @author chung
+ */
+public class Yum_ChungUn_Clics {
+
+    
+    public static void main(String[] args) {
+       Scanner entrada= new Scanner (System.in);
+       int clics; 
+       double costo_promedio,costo_ind,isv;
+       
+       System.out.println("Ingrese la cantidad de clics en su anuncio: ");
+       clics= entrada.nextInt();
+       
+      if(clics<=20){
+       costo_ind= 0.80;
+      }
+      
+    }
+    
+}

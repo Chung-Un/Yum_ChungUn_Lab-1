011d9db691f7369c15a4431b2d75c56258e1b73b
Chung-Un <chungunyum23@gmail.com>
2024-10-18 08:33:18 -0600

    Scanner
    Estableciendo un scanner para la entrada de datos

---
+++ Yum_ChungUn_Variables/src/yum_chungun_planilla_de_empleados/Yum_ChungUn_Planilla_de_empleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yum_chungun_planilla_de_empleados;
+
+import java.util.Scanner;
+
+public class Yum_ChungUn_Planilla_de_empleados {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+    
+}
